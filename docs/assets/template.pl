4851efc801ee7a09db57e2ff23d8d0dbc3752c0c
Flavio Poletti <flavio@polettix.it>
2022-01-15 19:58:02 +0100

    Add template file in Perl

--- docs/assets/template.pl
+++ docs/assets/template.pl
@@ -1,42 +1,3 @@
----
-# Feel free to add content and custom Front Matter to this file.
-# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
-
-title: 'App::Easer Tutorials'
-layout: default
----
-
-# Getting started
-
-Let's get started with [App::Easer][].
-
-## Installation
-
-[App::Easer][] is a regular [Perl][] module; here are a few hints for
-[Installing Perl Modules][] in case of need.
-
-Anyway, the goal of [App::Easer][] is to be confined into a single
-[Perl][] file that can be easily embedded into an application, in case of
-need. Think [App::FatPacker][]. For this reason, nothing prevents people
-from getting the module's file directly, e.g. the very latest (and
-possibly buggy, use at your own risk!) version in GitHub [here][latest].
-
-After making sure the module's file (contents) can be "seen" by your
-program, it suffices to `use` it. The suggestion is to import *at least*
-the `run` function, although the `d` (*dump on standard error*) function
-can come handy for debugging too.
-
-```perl
-use App::Easer qw< run d >;
-```
-
-Done! We're ready to move on.
-
-## A basic template
-
-This basic template can get us started ([download][]):
-
-```perl
 #!/usr/bin/env perl
 use v5.24;
 use warnings;
@@ -138,11 +99,3 @@ sub bar ($general, $config, $args) {
     say defined($config->{galook}) ? $config->{galook} : '*undef*';
     return;
 }
-```
-
-[App::Easer]: https://metacpan.org/pod/App::Easer
-[Installing Perl Modules]: https://github.polettix.it/ETOOBUSY/2020/01/04/installing-perl-modules/
-[Perl]: https://www.perl.org/
-[App::FatPacker]: https://metacpan.org/pod/App::FatPacker
-[latest]: https://raw.githubusercontent.com/polettix/App-Easer/main/lib/App/Easer.pm
-[download]: {{ '/assets/template.pl' | prepend: site.baseurl }}
