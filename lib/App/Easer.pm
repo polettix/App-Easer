408d69058c66a9d00147b20c2df778d590b32119
Flavio Poletti <flavio@polettix.it>
2022-02-03 23:58:38 +0100

    0.011

--- lib/App/Easer.pm
+++ lib/App/Easer.pm
@@ -3,7 +3,7 @@ use v5.24;
 use warnings;
 use experimental qw< signatures >;
 no warnings qw< experimental::signatures >;
-{ our $VERSION = '0.010' }
+{ our $VERSION = '0.011' }
 
 use Exporter 'import';
 our @EXPORT_OK = qw< d run >;
