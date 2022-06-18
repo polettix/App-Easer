f4f14cedfc3c344936fac570e430889889740bcd
Flavio Poletti <flavio@polettix.it>
2022-06-18 18:07:34 +0200

    2.002

--- lib/App/Easer/V1.pm
+++ lib/App/Easer/V1.pm
@@ -3,7 +3,7 @@ use v5.24;
 use warnings;
 use experimental qw< signatures >;
 no warnings qw< experimental::signatures >;
-{ our $VERSION = '2.001001' }
+{ our $VERSION = '2.002' }
 
 use parent 'Exporter';
 our @EXPORT_OK = qw< d run appeaser_api >;
