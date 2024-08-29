e0b9890eafe7dc05375673d4d7e7af04f5392002
Flavio Poletti <flavio@polettix.it>
2024-08-29 16:45:56 +0000

    2.007002

--- lib/App/Easer/V2.pm
+++ lib/App/Easer/V2.pm
@@ -3,7 +3,7 @@ use v5.24;
 use warnings;
 use experimental qw< signatures >;
 no warnings qw< experimental::signatures >;
-{ our $VERSION = '2.007001' }
+{ our $VERSION = '2.007002' }
 use Carp;
 
 use parent 'Exporter';
